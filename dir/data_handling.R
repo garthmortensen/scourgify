e35681681b0835b9597553551fb117b81a03a965
Garth Mortensen <mortensengarth@hotmail.com>
2023-07-23 08:35:18 -0500

    testing

--- dir/data_handling.R
+++ dir/data_handling.R
@@ -2,7 +2,7 @@ connect_to_database <- function(db_name) {
   db_location <- file.path("standup_db", db_name)
 
 
-  con <- dbConnect(RSQLite::SQLite(), dbname=db_location)
+  con <- dbConnect(RSQLite::SQLite(), dbname = db_location)
   print(glue::glue("db_location: {db_location}\n\n"))
   return(con)
 }
