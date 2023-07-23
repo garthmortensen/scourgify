e209dc9b2595277599853dcea97cd74fbf339013
Garth Mortensen <mortensengarth@hotmail.com>
2023-07-23 14:22:12 -0500

    add sql files to commit

---
+++ dir/eeeeee.sql
@@ -0,0 +1,8 @@
+-- lets see what happens
+select
+    columna,
+    columnv,
+    columnc
+        as whatever, columng
+from thistable1
+;
---
+++ dir/wowow.sql
@@ -0,0 +1,8 @@
+-- lets see what happens
+select
+    column1,
+    column2,
+    column3
+        as whatever, column4
+from thistable
+;
